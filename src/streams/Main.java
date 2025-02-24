5dc4ddb952346215e5bdf11646a81ab83248b314
mrkotyaka <unfatum@gmail.com>
2025-02-24 20:51:32 +0300

    add create two new file

--- src/streams/Main.java
+++ src/streams/Main.java
@@ -6,8 +6,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         File dir = new File("my_dir");
-//        File file1 = new File("my_dir", "file1.txt");
-        File file2 = new File(dir, "file2.txt");
+        File file1 = new File("my_dir", "file1.txt");
 
         if (dir.mkdir()) {
             System.out.println("Directory created");
@@ -15,7 +14,19 @@ public class Main {
             System.out.println("Directory not created");
         }
 
+        try {
+            if (dir.exists()){
+                if (file1.createNewFile()){
+                    System.out.println("File created");
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
         File new_my_dir =  new File("new_my_dir");
+        File file2 = new File(new_my_dir, "file2.txt");
         if (dir.renameTo(new_my_dir)) {
             System.out.println("File renamed");
         } else   {
