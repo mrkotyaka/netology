17603910c0d4c7ab85471cf3bdcedaa40613548f
mrkotyka <unfatum@gmail.com>
2025-02-24 20:27:49 +0300

    learning streams

--- src/streams/Main.java
+++ src/streams/Main.java
@@ -5,8 +5,8 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) {
         File dir = new File("my_dir");
-        File file1 = new File("my_dir", "file1.txt");
-        File file2 = new File(dir, "file2.txt");
+//        File file1 = new File("my_dir", "file1.txt");
+//        File file2 = new File(dir, "file2.txt");
 
         if (dir.mkdir()) {
             System.out.println("Directory created");
